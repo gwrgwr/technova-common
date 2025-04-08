ed758c31dc4170cf6623fdb1e801b128fa4e7458
gwrgwr <murilomassa72@gmail.com>
2025-04-08 12:41:32 -0300

    add: create RabbitVendorConstants class with vendor-related constants

---
+++ src/main/java/com/technova/vendor/constants/RabbitVendorConstants.java
@@ -0,0 +1,13 @@
+package com.technova.vendor.constants;
+
+public class RabbitVendorConstants {
+
+    public static final String VENDOR_EXCHANGE = "vendor-exchange";
+    public static final String VENDOR_SAVE_REQUEST_QUEUE = "vendor-save-request";
+    public static final String VENDOR_LOGIN_REQUEST_QUEUE = "vendor-login-request";
+    public static final String VENDOR_SAVE_REQUEST_ROUTING_KEY = "vendor.save";
+    public static final String VENDOR_LOGIN_REQUEST_ROUTING_KEY = "vendor.login";
+
+    public RabbitVendorConstants() {
+    }
+}
